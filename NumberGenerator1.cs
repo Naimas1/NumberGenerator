9cca029a54ce2921b68cf4676d745a230c94a756
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2024-04-06 10:21:35 +0300

    NumberGenerator1.cs

--- NumberGenerator1.cs
+++ NumberGenerator1.cs
@@ -1,12 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NumberGenerator
+namespace PrimeNumberGenerator
 {
-    internal class NumberGenerator1
+    public partial class Form1 : Form
     {
+        private Thread primeThread;
+        private int lowerBound;
+        private int upperBound;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GeneratePrimes()
+        {
+            int currentNumber = lowerBound <= 2 ? 2 : lowerBound;
+
+            while (true)
+            {
+                bool isPrime = true;
+
+                // Перевірка чи число є простим
+                for (int i = 2; i <= Math.Sqrt(currentNumber); i++)
+                {
+                    if (currentNumber % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    // Оновлення відображення в головному потоці
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        return listBox1.Items.Add(currentNumber);
+                    }));
+                }
+
+                if (upperBound != 0 && currentNumber >= upperBound)
+                {
+                    break;
+                }
+
+                currentNumber++;
+            }
+        }
+
+        private void Invoke(MethodInvoker methodInvoker)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxLowerBound.Text, out lowerBound) && int.TryParse(textBoxUpperBound.Text, out upperBound))
+            {
+                primeThread = new Thread(GeneratePrimes);
+                primeThread.IsBackground = true; // Потік буде закриватися, якщо головний потік закриється
+                primeThread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Введіть правильні значення для нижньої та верхньої межі діапазону.");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (primeThread != null && primeThread.IsAlive)
+            {
+                primeThread.Abort();
+            }
+        }
     }
 }
